bfc9a293a34d6b4ccb129332186d71b3e347e479
Harrison Malone <harrison.malone2@gmail.com>
2019-08-30 09:21:18 +1000

    initial commit

---
+++ 01-gems.rb
@@ -0,0 +1,13 @@
+# 1. Define a method called temperature_colour
+
+# 2. Call the method and pass it an argument that represents celsius as an integer
+
+# 3. Write some control from logic that returns different strings
+
+# a. If the celsius is lower than 10 puts it's freezing
+
+# b. If the celsius is higher than 10 but lower than 30 puts it's pretty good 🤙
+
+# c. If the celsius is higher than 30 puts it's boiling
+
+# d. Use the colorize gem to make the different strings different colors: blue for "its freezing", yellow for "it's pretty good 🤙" and red for "its boiling"
