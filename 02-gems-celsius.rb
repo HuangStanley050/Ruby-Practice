7f0179556c17a2b996d36162c32156e482012cd0
Harrison Malone <harrison.malone2@gmail.com>
2019-08-30 09:46:36 +1000

    edit

--- 02-gems-celsius.rb
+++ 02-gems-celsius.rb
@@ -11,3 +11,6 @@
 # c. If the celsius is higher than 30 puts it's boiling
 
 # d. Use the colorize gem to make the different strings different colors: blue for "its freezing", yellow for "it's pretty good 🤙" and red for "its boiling"
+
+# e. Move the method call into another file named app.rb, use require_relative to make this work
+
